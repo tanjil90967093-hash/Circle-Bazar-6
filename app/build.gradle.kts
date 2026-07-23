dcee8dc9439a25468447d5c9bf0092faafb6fcc8
tanjil90967093-hash <tanjil90967093@gmail.com>
2026-07-23 18:21:47 +0600

    build: update compileSdk and remove build workflow
    Update compileSdk configuration to use minor API level 1 and remove the obsolete CI build workflow.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk = 36
+  compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
     applicationId = "com.aistudio.circlebazar.cbeadb"
